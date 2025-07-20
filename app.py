f483e10d3515c64f3b326b458a46da80c3000b79
Prerna rai <148607709+Prerna15Rai@users.noreply.github.com>
2025-07-20 12:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import dash
-import dash_html_components as html
 from dash import html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 from dash import dcc
 from dash.dependencies import Input,Output
 import plotly.express as px
